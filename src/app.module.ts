24562fa5f2981b68b460d14751bdf085db992053
harley <a1209082835@gmail.com>
2023-12-24 20:13:31 +0800

    链接数据库

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,16 +18,15 @@ import envConfig from '../config/env';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: async (config: ConfigService) => {
+      useFactory: async (config) => {
         const isProd = config.get('NODE_ENV') === 'production';
-        const dbConfig = config.get('database');
         return {
           type: 'mysql', // 数据库类型
-          host: dbConfig.host, // 主机，默认为localhost
-          port: dbConfig.port,
-          username: dbConfig.username,
-          password: dbConfig.password,
-          database: dbConfig.database,
+          host: config.get('DB_HOST'), // 主机，默认为localhost
+          port: config.get('DB_PORT'), // 端口，默认为3306
+          username: 'root' || config.get('DB_USER'), // 用户名
+          password: '123456' || config.get('DB_PASSWORD'), // 密码
+          database: config.get('DB_DATABASE'), // 数据库名
           entities: [__dirname + '/**/*.entity{.ts,.js}'],
           synchronize: !isProd,
           timezone: '+08:00', // 服务器上配置的时区
