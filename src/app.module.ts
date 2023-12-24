16d842ae3f5404beec7c2b0a144d6aaf91177456
harley <a1209082835@gmail.com>
2023-12-24 16:43:02 +0800

    docs(添加基本注释): 接口统一添加api前缀

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [], // 导入模块,使用其他模块的服务,比如数据库,缓存等,
+  controllers: [AppController], // 控制器,处理请求和响应,返回数据
+  providers: [AppService], // 服务提供者
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 
-@Injectable()
+@Injectable() // 修饰器, 用于标记类, 使其成为 Nest 可以管理的 provider, 也就是可以被依赖注入的对象
+// 修饰后，通过appModule注册，就可以在其他地方使用了
 export class AppService {
   getHello(): string {
     return 'Hello World!';
