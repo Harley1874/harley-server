823dc34e0ca436af572dce539a22267385402c8c
harley <a1209082835@gmail.com>
2023-12-24 16:48:00 +0800

    添加posts模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [], // 导入模块,使用其他模块的服务,比如数据库,缓存等,
+  imports: [PostsModule], // 导入模块,使用其他模块的服务,比如数据库,缓存等,
   controllers: [AppController], // 控制器,处理请求和响应,返回数据
   providers: [AppService], // 服务提供者
 })
